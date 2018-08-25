d385288cabd533a8c4c232f69ee756f14cd2437b
yasu <yasuBrosK@gmail.com>
2018-08-25 17:07:53 +0900

    ディレクトリ生成処理実装

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"os/user"
 	"path/filepath"
 
@@ -10,15 +11,26 @@ import (
 	"github.com/ysbrothersk/spoitgo/spoitgo"
 )
 
+var u, uerror = user.Current()
+var saveDir = filepath.Join(u.HomeDir, "Pictures", "SpoitGo")
+
+func init() {
+	if uerror != nil {
+		panic(uerror)
+	}
+}
+
 func main() {
 	logo.Print()
 
-	u, err := user.Current()
+	isCreate, err := createDirIfNothing(saveDir)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	saveDir := filepath.Join(u.HomeDir, "Pictures", "SpoitGo")
+	if isCreate {
+		fmt.Println("create new directory:", saveDir)
+	}
 
 	newCount, err := spoitgo.CloneSpotlightImage(saveDir)
 	if err != nil {
@@ -37,6 +49,11 @@ func main() {
 	fmt.Println("Total:", total)
 }
 
+func isExist(dir string) bool {
+	_, err := os.Stat(dir)
+	return err == nil
+}
+
 // getTotalCount ディレクトリ内ファイルの総数を返却します
 func getTotalCount(dir string) (int, error) {
 	files, err := ioutil.ReadDir(dir)
@@ -45,4 +62,21 @@ func getTotalCount(dir string) (int, error) {
 	}
 
 	return len(files), nil
+}
+
+// createDirIfNothing dirパスにディレクトリがない場合ディレクトリを生成します
+// ディレクトリを生成した場合はtrue、しなかった場合はfalseを返却します。
+// エラーが発生した場合はerrを返却します
+func createDirIfNothing(dir string) (bool, error) {
+	if isExist(dir) {
+		return false, nil
+	}
+
+	// ディレクトリの生成処理
+	// TODO: この権限のbyte値をよく調べる
+	err := os.MkdirAll(dir, 0777)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
